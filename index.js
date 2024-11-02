fafb9bc44d7282b33f60380432a18991d1973689
liuguoyi <liu.guoy@northeasern.edu.cn>
2024-11-01 21:10:39 -0700

    bobby-ecommerce-backend-v1

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "https://bobby-ecommerce-frontend.vercel.app/",
+    origin: "https://bobby-ecommerce-frontend.vercel.app",
     credentials: true,
   })
 );
